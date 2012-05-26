09c9cb9b1e54d8975601b3743c7cac5abf2096ba
zeubea <zeubea@gmail.com>
2012-05-26 14:55:09 +0200

    ics mods

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,42 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
 
 # WARNING: This line must come *before* including the proprietary
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
-USE_CAMERA_STUB := true
+USE_CAMERA_STUB := false
+BOARD_USE_FROYO_LIBCAMERA := true
+JS_ENGINE:=v8
 
 # inherit from the proprietary version
 -include vendor/htc/legend/BoardConfigVendor.mk
 
-
-TARGET_BOARD_PLATFORM := msm7k
-TARGET_ARCH_VARIANT := armv6-vfp
-TARGET_CPU_ABI := armeabi-v6l
-TARGET_CPU_ABI2 := armeabi
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-TARGET_BOOTLOADER_BOARD_NAME := legend
-
+TARGET_SPECIFIC_HEADER_PATH := device/htc/legend/include
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
-BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+TARGET_BOARD_PLATFORM := msm7x27
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+
+TARGET_BOOTLOADER_BOARD_NAME := legend
+TARGET_PROVIDES_INIT_TARGET_RC := true
 
 TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-BOARD_NO_RGBX_8888 := true
-
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-BOARD_USE_FROYO_LIBCAMERA := true
-
-BOARD_USES_QCOM_LIBS := true
-BOARD_USES_QCOM_LIBRPC := true
-BOARD_USES_QCOM_GPS := true
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 20000
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := legend
 
 BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
 BOARD_WLAN_DEVICE := wl1271
@@ -59,12 +57,32 @@ WIFI_FIRMWARE_LOADER := wlan_loader
 WIFI_EXT_MODULE_PATH := /system/lib/modules/sdio.ko
 WIFI_EXT_MODULE_NAME := sdio
 
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_BASE := 0x12c00000
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 4735
+
+BOARD_VENDOR_USE_AKMD := akm8973
+
+BOARD_EGL_CFG := device/htc/legend/prebuilt/egl.cfg
+
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+
 BOARD_USE_NEW_LIBRIL_HTC := true
 
-BOARD_EGL_CFG := device/htc/legend/files/egl.cfg
+BOARD_NO_RGBX_8888 := true
 
-BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
-BOARD_KERNEL_BASE := 0x12C00000
+
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := legend
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 20000
+BOARD_USES_QCOM_LIBRPC := true
+
 
 #$ adb shell cat /proc/mtd
 #dev:    size   erasesize  name
@@ -74,7 +92,8 @@ BOARD_KERNEL_BASE := 0x12C00000
 #mtd3: 0f000000 00020000 "system"
 #mtd4: 02800000 00020000 "cache"
 #mtd5: 0b920000 00020000 "userdata"
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x003e0000
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x003e0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00300000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0f000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0b920000
@@ -82,6 +101,32 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0b920000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+TARGET_PREBUILT_KERNEL := device/htc/legend/prebuilt/kernel
+LOCAL_KERNEL := device/htc/legend/prebuilt/kernel
+
+#TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/legend/prebuilt/recovery_kernel
+#BOARD_USES_RECOVERY_CHARGEMODE := true
+TARGET_RECOVERY_INITRC := device/htc/legend/init.recovery.rc
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
+
+# Fix for Atmel touchscreens; trackball button
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+BOARD_USE_LEGACY_TRACKPAD := true
 
 WITH_DEXPREOPT := true
-JS_ENGINE := v8
+HTTP := chrome
+
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DREFRESH_RATE=60
+USE_OPENGL_RENDERER := true
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+TARGET_USES_GENLOCK := true
+
+# Camera 
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+
+
--- legend.mk
+++ legend.mk
@@ -16,7 +16,7 @@
 
 # Kernel Targets
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/htc/legend/files/kernel
+LOCAL_KERNEL := device/htc/legend/prebuilt/kernel
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif # TARGET_PREBUILT_KERNEL
@@ -30,20 +30,58 @@ DEVICE_PACKAGE_OVERLAYS := device/htc/legend/overlay
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    device/htc/legend/files/legend-keypad.kl:system/usr/keylayout/legend-keypad.kl \
-    device/htc/legend/files/legend-keypad.kcm.bin:system/usr/keychars/legend-keypad.kcm.bin \
-    device/htc/legend/files/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl
+    device/htc/legend/prebuilt/legend-keypad.kl:system/usr/keylayout/legend-keypad.kl \
+    device/htc/legend/prebuilt/legend-keypad.kcm.bin:system/usr/keychars/legend-keypad.kcm.bin \
+    device/htc/legend/prebuilt/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/legend/prebuilt/usr/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
+    device/htc/legend/prebuilt/usr/keylayout/atmel-touchscreen.kl:system/usr/keylayout/atmel-touchscreen.kl
+
+
+# Input device calibration files
+PRODUCT_COPY_FILES += \
+    device/htc/legend/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+    device/htc/legend/prebuilt/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
+    device/htc/legend/prebuilt/usr/idc/curcial-oj.idc:system/usr/idc/curcial-oj.idc
+
+
+#PRODUCT_COPY_FILES += \
+#    device/htc/legend/prebuilt/init.legend.rc:root/init.legend.rc \
+#    device/htc/legend/prebuilt/ueventd.legend.rc:root/ueventd.legend.rc
+
+
+
+
+# Qualcomm ICS Adreno200 drivers  
+PRODUCT_COPY_FILES += \
+    device/htc/legend/prebuilt/lib/libC2D2.so:system/lib/libC2D2.so \
+    device/htc/legend/prebuilt/lib/libgsl.so:system/lib/libgsl.so \
+    device/htc/legend/prebuilt/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    device/htc/legend/prebuilt/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    device/htc/legend/prebuilt/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    device/htc/legend/prebuilt/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    device/htc/legend/prebuilt/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    device/htc/legend/prebuilt/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    device/htc/legend/prebuilt/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    device/htc/legend/prebuilt/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    device/htc/legend/prebuilt/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    device/htc/legend/prebuilt/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    device/htc/legend/prebuilt/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    device/htc/legend/prebuilt/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw
+
 
 PRODUCT_COPY_FILES += \
-    device/htc/legend/files/init.legend.rc:root/init.legend.rc \
-    device/htc/legend/files/ueventd.legend.rc:root/ueventd.legend.rc
+    device/htc/legend/init.legend.rc:root/init.legend.rc \
+    device/htc/legend/init.legend.usb.rc:root/init.legend.usb.rc \
+    device/htc/legend/ueventd.legend.rc:root/ueventd.legend.rc
+
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/lib/libhtc_ril.so \
+    ro.ril.enable.dtm=1 \
     ro.sf.lcd_density=160 \
     wifi.interface=tiwlan0 \
-    wifi.supplicant_scan_interval=15 \
-    ro.ril.def.agps.mode=2 \
+    ro.ril.def.agps.mode=1 \
     ro.ril.ecc.HTC-ELL=92,93,94 \
     ro.ril.ecc.HTC-WWE=999 \
     ro.ril.enable.a52.HTC-ITA=1 \
@@ -51,11 +89,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.hsdpa.category=8 \
     ro.ril.hsupa.category=5 \
     ro.ril.hsxpa=2 \
+    mobiledata.interfaces=rmnet0,rmnet1,rmnet2,gprs,ppp0 \
+    wifi.supplicant_scan_interval=15 \
     ro.ril.disable.fd.plmn.prefix=23402,23410,23411 \
-    ro.opengles.version=131072
+    ro.opengles.version=131072 \
+    ro.telephony.ril.v3=signalstrength 
 
 # Default network type.
 # 0 => WCDMA preferred.
+# 1 => only GSM
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=0
 
@@ -86,42 +128,76 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.locationfeatures = 1 \
     ro.setupwizard.mode=OPTIONAL \
     ro.setupwizard.enable_bypass=1 \
+    ro.media.dec.aud.wma.enabled=1 \
+    ro.media.dec.vid.wmv.enabled=1 \
     dalvik.vm.dexopt-flags=m=y \
-    net.bt.name=Android \
-    ro.config.sync=yes
+    net.bt.name=Legend \
+    ro.config.sync=yes \
+    persist.sys.usb.config=mass_storage,adb \
+    com.qc.hardware=true \
+    com.qc.hdmi_out=false \
+    persist.sys.tbwake=1 \
+    debug.sf.hw=1 \
+    debug.qctwa.statusbar=1 \
+    debug.qctwa.preservebuf=1 \
+    debug.camcorder.disablemeta=1	
 
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
 
 PRODUCT_COPY_FILES += \
-    device/htc/legend/files/etc/media_profiles.xml:system/etc/media_profiles.xml
+    device/htc/legend/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
-    device/htc/legend/files/etc/fstab:system/etc/fstab \
-    device/htc/legend/files/etc/pvasflocal.cfg:system/etc/pvasflocal.cfg \
-    device/htc/legend/files/etc/vold.fstab:system/etc/vold.fstab
+	device/htc/legend/prebuilt/etc/vold.fstab:system/etc/vold.fstab 
+
+#PRODUCT_COPY_FILES += \
+#    device/htc/legend/vold.fstab:system/etc/vold.fstab
 
 PRODUCT_COPY_FILES += \
-    device/htc/legend/files/modules/sdio.ko:system/lib/modules/sdio.ko \
-    device/htc/legend/files/modules/tiwlan_drv.ko:system/lib/modules/tiwlan_drv.ko \
-    device/htc/legend/files/modules/tiap_drv.ko:system/lib/modules/tiap_drv.ko
+    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    device/htc/legend/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd
+
+PRODUCT_COPY_FILES += \
+    device/htc/legend/prebuilt/modules/sdio.ko:system/lib/modules/sdio.ko \
+    device/htc/legend/prebuilt/modules/tiwlan_drv.ko:system/lib/modules/tiwlan_drv.ko \
+    device/htc/legend/prebuilt/modules/tiap_drv.ko:system/lib/modules/tiap_drv.ko
 
 PRODUCT_PACKAGES += \
     librs_jni \
-    copybit.msm7k \
-    gralloc.msm7k \
-    gps.legend \
-    sensors.legend \
     lights.legend \
+    gralloc.msm7x27 \
+    copybit.msm7x27 \
+    sensors.legend \
+    audio.primary.legend \
+    audio_policy.legend \
+    audio.a2dp.default \
+    gps.legend \
+    libmm-omxcore \
     libOmxCore \
-    libmm-omxcore
+    libstagefrighthw \
+    hwcomposer.default \
+    hwcomposer.msm7x27 \
+    libgenlock \
+    libmemalloc \
+    libtilerenderer \
+    libQcomUI \
+    com.android.future.usb.accessory \
+    e2fsck \
+    camera.msm7x27
+
 
 #WiFi stuff
 PRODUCT_PACKAGES += \
@@ -134,13 +210,23 @@ PRODUCT_PACKAGES += \
 
 #HotSpot
 PRODUCT_PACKAGES += \
-    tiap_loader \
+   tiap_loader \
     tiap_cu \
     tiwlan_ap.ini \
     hostap \
     hostapd.conf
 
-PRODUCT_LOCALES += mdpi
+
+#media profiles and capabilities spec
+$(call inherit-product, device/htc/legend/media_a1026.mk)
+
+
+# Prebuilt libraries that are needed to build open-source libraries
+#PRODUCT_COPY_FILES += \
+#    device/htc/legend/prebuilt/lib/libcamera.so:obj/lib/libcamera.so
+
+
+# PRODUCT_LOCALES += mdpi
 
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 $(call inherit-product, device/htc/common/common.mk)
