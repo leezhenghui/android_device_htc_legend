2775deed7191f1799b9c1797ea1c1bc87f78dacb
zeubea <zeubea@gmail.com>
2012-11-27 00:34:31 +0100

    Using msm7x27-tiwlan-common instead of msm7x27-common.

--- legend.mk
+++ legend.mk
@@ -108,7 +108,7 @@ PRODUCT_COPY_FILES += \
     device/htc/legend/prebuilt/lib/libcamera.so:obj/lib/libcamera.so
 
 # inherit from common msm7x27
-$(call inherit-product, device/htc/msm7x27-common/msm7x27.mk)
+$(call inherit-product, device/htc/msm7x27-tiwlan-common/msm7x27.mk)
 
 $(call inherit-product, build/target/product/full_base_telephony.mk)
 
